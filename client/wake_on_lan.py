0d86e7eef7920fb723dc41da05db7c8548827429
MrLaki5 <mimar1207@gmail.com>
2019-05-19 23:29:54 +0200

    bugs fixed

--- client/wake_on_lan.py
+++ client/wake_on_lan.py
@@ -1,12 +1,9 @@
 import paho.mqtt.client as mqtt
 import os
 
-mqtt_port = int(os.environ['MQTT_PORT'])
-wake_mac = os.environ['WAKE_MAC']
-
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
+def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+ str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -17,7 +14,7 @@ def on_connect(client, userdata, rc):
 client = mqtt.Client(client_id="10e6421e-7c5b-49d6-8319-a9c19dbffad6", clean_session=True)
 client.on_connect = on_connect
 
-client.connect("192.168.1.103", mqtt_port, 60)
+client.connect("localhost", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
--- pi_command_executor/src/command_executor.py
+++ pi_command_executor/src/command_executor.py
@@ -1,14 +1,16 @@
 import paho.mqtt.client as mqtt
 import os
-import magic_pakacge
+import magic_package
+import logging
 
+logging.basicConfig(level=logging.INFO)
 mqtt_port = int(os.environ['MQTT_PORT'])
 wake_mac = os.environ['WAKE_MAC']
 
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-    print("Connected with result code "+ str(rc))
+def on_connect(client, userdata, flags, rc):
+    logging.info("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("devices/wake_on_lan")
@@ -17,17 +19,17 @@ def on_connect(client, userdata, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     message = msg.payload.decode("UTF-8")
+    logging.info("message")
     if msg.topic == "devices/wake_on_lan":
-        magic_pakacge.send_package(wake_mac)
+        magic_package.send_package(wake_mac)
         print(message)
 
 
 client = mqtt.Client(client_id="20e6421e-7c5b-49d6-8319-a9c19dbffad6", clean_session=True)
 client.on_connect = on_connect
 client.on_message = on_message
-
-client.connect("broker", mqtt_port, 60)
-
+client.connect("localhost", mqtt_port, 60)
+logging.info("connecting1")
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
