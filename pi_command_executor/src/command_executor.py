786c48ddad7805c680dfe7e0d887a6268c5eed98
MrLaki5 <mimar1207@gmail.com>
2019-05-19 19:52:58 +0200

    adding wake on lan

--- pi_command_executor/src/command_executor.py
+++ pi_command_executor/src/command_executor.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import os
+import magic_pakacge
 
 mqtt_port = os.environ['MQTT_PORT']
 
@@ -8,11 +9,14 @@ def on_connect(client, userdata, rc):
     print("Connected with result code "+ str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("devices/wake_on_lan")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+ str(msg.payload))
+    message = msg.payload.decode("UTF-8")
+    if msg.topic == "devices/wake_on_lan":
+        magic_pakacge.
+        print(message)
 
 client = mqtt.Client()
 client.on_connect = on_connect
