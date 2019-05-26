8f53c1d5629747a54cfd6df703261421150b8a94
MrLaki5 <mimar1207@gmail.com>
2019-05-26 13:12:53 +0200

    change to async mqtt on android

--- Slacker-app/app/src/main/java/com/example/slacker_app/MainActivity.java
+++ Slacker-app/app/src/main/java/com/example/slacker_app/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.slacker_app;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -19,16 +23,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         Button wakeUpButt = findViewById(R.id.wakeUpButton);
         wakeUpButt.setOnClickListener(wakeButtListener);
+        Button lastestYTButt = findViewById(R.id.playLatestYT);
+        lastestYTButt.setOnClickListener(playLatestYTListener);
+    }
+
+    @Override
+    protected void onStart() {
         startMqtt();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mqttHelper.stop();
+        super.onStop();
     }
 
     private View.OnClickListener wakeButtListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            mqttHelper.publishMessage("devices/wake_on_lan", "hello");
+            boolean result = mqttHelper.publishMessage("devices/wake_on_lan", "hello");
+            if (result){
+                Toast.makeText(MainActivity.this, "Wake sent", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private View.OnClickListener playLatestYTListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String channelName = ((EditText)findViewById(R.id.YTChannelName)).getText().toString();
+            String message = "{\"command\": \"youtube_latest\", \"channel\": \"" + channelName + "\"}";
+            boolean result = mqttHelper.publishMessage("devices/50:e5:49:1a:ee:e3/command", message);
+            if (result){
+                Toast.makeText(MainActivity.this, "Playing latest video of channel " + channelName, Toast.LENGTH_SHORT).show();
+            }
         }
     };
 
@@ -37,12 +70,12 @@ public class MainActivity extends AppCompatActivity {
         mqttHelper.setCallbackAndConnect(new MqttCallbackExtended() {
             @Override
             public void connectComplete(boolean b, String s) {
-
+                ((TextView) findViewById(R.id.connectionStatus)).setText("Connection: established");
             }
 
             @Override
             public void connectionLost(Throwable throwable) {
-
+                ((TextView) findViewById(R.id.connectionStatus)).setText("Connection: not established");
             }
 
             @Override
--- Slacker-app/app/src/main/java/com/example/slacker_app/MqttHelper.java
+++ Slacker-app/app/src/main/java/com/example/slacker_app/MqttHelper.java
@@ -8,21 +8,26 @@ import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MqttHelper {
 
-    public MqttAndroidClient mqttAndroidClient;
+    public MqttAsyncClient mqttAndroidClient;
 
-    final String serverUri = "192.168.1.200:1883";
+    final String serverUri = "tcp://192.168.1.200:1883";
 
-    final String clientId = "ExampleAndroidClient";
+    final String clientId = "55e6421e-7c5b-49d6-8319-a9c19dbffad6";
 
     final String username = "xxxxxxx";
     final String password = "yyyyyyyyyy";
+
+    private MqttClientPersistence presistance = null;
 
     private IMqttActionListener mqttActionListener = new IMqttActionListener() {
         @Override
@@ -37,7 +42,12 @@ public class MqttHelper {
     };
 
     public MqttHelper(Context context){
-        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
+        try {
+            presistance = new MemoryPersistence();
+            mqttAndroidClient = new MqttAsyncClient(serverUri, clientId, presistance);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setCallbackAndConnect(MqttCallbackExtended callback) {
@@ -59,27 +69,40 @@ public class MqttHelper {
     }
 
 
-    public void subscribeToTopic(String topic, int qos) {
+    public boolean subscribeToTopic(String topic, int qos) {
         if(mqttAndroidClient.isConnected()) {
             try {
                 mqttAndroidClient.subscribe(topic, qos, null, mqttActionListener);
-
+                return true;
             } catch (MqttException ex) {
                 System.err.println("Exception whilst subscribing");
                 ex.printStackTrace();
             }
         }
+        return false;
     }
 
 
-    public void publishMessage(String topic, String message){
+    public boolean publishMessage(String topic, String message){
         if(mqttAndroidClient.isConnected()){
             MqttMessage mqttMessage = new MqttMessage();
             mqttMessage.setPayload(message.getBytes());
             try {
                 mqttAndroidClient.publish(topic, mqttMessage);
+                return true;
             }
             catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public void stop(){
+        if (mqttAndroidClient != null){
+            try {
+                mqttAndroidClient.disconnect();
+            } catch (MqttException e) {
                 e.printStackTrace();
             }
         }
